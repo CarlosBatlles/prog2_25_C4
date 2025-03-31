89e74198307124c2e07bb7adc5c58f25d42911cb
AAlexxis222 <aap168@alu.ua.es>
2025-03-31 14:26:50 +0200

    Ajuste clase coche
    Ajustada la clase coche tras la creacion de las bases de datos

--- source/models/coche.py
+++ source/models/coche.py
@@ -1,24 +1,52 @@
 
 '''Clase Coche para representar los coches disponibles para alquilar y sus características'''
 
-class Coche():
-    
-    def __init__(self, marca: str, categoria: str, nivel: str, precio_dia: float, disponible: bool = True):
+class Coche:
+    def __init__(self, id_coche, marca, modelo, matricula, categoria_tipo, categoria_precio, año, precio_diario, kilometraje, color, combustible, cv, plazas, disponible):
+        self.id_coche = id_coche
         self.marca = marca
-        self.categoria = categoria
-        self.nivel = nivel
-        self.precio_dia = precio_dia
+        self.modelo = modelo
+        self.matricula = matricula
+        self.categoria_tipo = categoria_tipo
+        self.categoria_precio = categoria_precio
+        self.año = año
+        self.precio_diario = precio_diario
+        self.kilometraje = kilometraje
+        self.color = color
+        self.combustible = combustible
+        self.cv = cv
+        self.plazas = plazas
         self.disponible = disponible
         
     def alquilar(self):
-        '''cambia disponible a false y devuelve true si se pudo alquilar, false si no'''
-        pass
+        ''' Marca el coche como no disponible'''
+        if not self.disponible:
+            raise ValueError(f"El coche {self.id_coche} ({self.marca} {self.modelo}) no esta disponible")
+        self.disponible = False
+        return True
+        
     
     def devolver(self):
-        '''cambia disponible a true'''
-        pass
+        ''' Marca el coche como disponible'''
+        if self.disponible:
+            raise ValueError(f"El coche {self.id_coche} ({self.marca} {self.modelo}) ya se encuentra disponible")
+        self.disponible = False
+    
     
     def get_info(self):
-        '''devuelve un str con los detalles del coche, para mostrar en coches disponibles'''
-        pass
+        '''Devuelve un string con los detalles del coche en un formato legible'''
+        return (f"Coche ID: {self.id_coche}\n"
+                f"Marca: {self.marca}\n"
+                f"Modelo: {self.modelo}\n"
+                f"Matrícula: {self.matricula}\n"
+                f"Categoría Tipo: {self.categoria_tipo}\n"
+                f"Categoría Precio: {self.categoria_precio}\n"
+                f"Año: {self.año}\n"
+                f"Precio por día: {self.precio_diario} EUR\n"
+                f"Kilometraje: {self.kilometraje} km\n"
+                f"Color: {self.color}\n"
+                f"Combustible: {self.combustible}\n"
+                f"Potencia: {self.cv} CV\n"
+                f"Plazas: {self.plazas}\n"
+                f"Disponible: {self.disponible}")
     
