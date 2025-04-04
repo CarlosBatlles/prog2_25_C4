35b3a5870b2a976a39037e792ff7ed1f47210763
AAlexxis222 <aap168@alu.ua.es>
2025-04-04 16:58:39 +0200

    Ordenacion y comienzo del desarrollo de empresa
    He ordenado el csv por uid para hacer mas facil la generacion de proximos ids, y he empezado a desarrollar empresa

--- source/empresa.py
+++ source/empresa.py
@@ -1,6 +1,6 @@
 
 # Clase Empresa que va a gestionar todo el alquiler de coches 
-
+import pandas as pd
 import datetime
 
 class Empresa():
@@ -9,6 +9,59 @@ class Empresa():
         self.coches = []
         self.usuarios = []
         self.alquileres = []
+    
+     # Metodos para cargar las bases de datos
+     
+    def cargar_coches(self,ruta_archivo):
+        ''' Carga los coches desde un archivo CSV'''
+        try:
+            self.coches = pd.read_csv('C:\Users\Alexis Angulo Polan\Documents\GitHub\prog2-25-C4\data\coches.csv')
+        except FileNotFoundError:
+            print(f'El archivo {ruta_archivo} no se encontró')
+        except Exception as e:
+            print(f'Error al cargar coches {e}')
+    
+    def cargar_usuarios(self,ruta_archivo):
+        try:
+            self.usuarios = pd.read_csv('C:\Users\Alexis Angulo Polan\Documents\GitHub\prog2-25-C4\data\clientes.csv')
+        except FileNotFoundError:
+            print(f'El archivo {ruta_archivo} no se encontró')
+        except Exception as e:
+            print(f'Error al cargar usuarios {e}')
+            
+    def cargar_alquileres(self,ruta_archivo):
+        try:
+            self.alquileres = pd.read_csv('C:\Users\Alexis Angulo Polan\Documents\GitHub\prog2-25-C4\data\alquileres.csv')
+        except FileNotFoundError:
+            print(f'El archivo {ruta_archivo} no se encontro')
+        except Exception as e:
+            print(f'Error al cargar alquileres {e}')
+            
+    # Metodos para generar IDs de usuario y coche automaticamente  
+    
+    def generar_id_usuario(self):
+        '''Genera un ID unico para un nuevo usuario'''
+        df = self.cargar_usuarios
+        if df.empty:
+            return 'U001'
+        ultimo_id = df['id_usuario'].iloc[-1]
+        num = int(ultimo_id[1:]) + 1 # Extraer el número y sumar 1
+        return f'U{num:03d}' # Formato U001, U002, etc.
+    
+    def generar_id_alquiler(self):
+        df = self.cargar_alquileres
+        if df.empty:
+            return 'A001'
+        ultimo_id = df['id_alquiler'].iloc[-1]
+        num = int(ultimo_id[1:]) + 1
+        return f'A{num:03d}'
+    
+    def generar_id_coche(self):
+        df = self.cargar_coches
+        ultimo_id = df['id'].iloc[-1]
+        num = int(ultimo_id[1:]) + 1
+        return f'UID{num:02d}'
+        
     
     def registrar_coche(self,marca, categoria, nivel, precio_dia):
         # crear objeto Coche y añadirlo a self.coches
