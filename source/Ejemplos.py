96ff1ff135e32cf1f73b17f46cad78a13eaafc6b
AAlexxis222 <aap168@alu.ua.es>
2025-04-08 14:20:55 +0200

    Update Ejemplos.py

--- source/Ejemplos.py
+++ source/Ejemplos.py
@@ -1,19 +1,19 @@
 """ Ejemplos de la API"""
 
 def menu():
-print("\n Bienvenido a RentalCars:")
-print("1. Login")
-print("2. Signup")
-print("3. Entrar como invitado")
-print("4. Buscar Coches (como cliente)")
-print("5. Alquilar Coches (como cliente)")
-print("6. Ver Historial de Alquileres (como cliente)")
-print("7. Gestionar coches (como admin)")
-print("8. Consultar Bases de Datos (como admin)")
-print("9. Generar informes (como admin)")
-print("10. Buscar Coches (como invitado)")
-print("11. Alquilar Coches (como invitado)")
-print("0. Salir")
+    print("\n Bienvenido a RentAcar:")
+    print("1. Login")
+    print("2. Signup")
+    print("3. Entrar como invitado")
+    print("4. Buscar Coches (como cliente)")
+    print("5. Alquilar Coches (como cliente)")
+    print("6. Ver Historial de Alquileres (como cliente)")
+    print("7. Gestionar coches (como admin)")
+    print("8. Consultar Bases de Datos (como admin)")
+    print("9. Generar informes (como admin)")
+    print("10. Buscar Coches (como invitado)")
+    print("11. Alquilar Coches (como invitado)")
+    print("0. Salir")
 
 # ----------------------------
 # Bucle principal
