42bb7058bbb877c826947778e45569280fd7cdd0
CarlosBatlles <cbo17@alu.ua.es>
2025-04-09 18:50:41 +0200

    Creación de submenús
    División del menú en varias partes para facilitar la interfaz

--- source/Ejemplos.py
+++ source/Ejemplos.py
@@ -1,37 +1,67 @@
 """ Ejemplos de la API"""
+from os import login_tty
+
 import requests
 
 BASE_URL="http://127.0.0.1:5050" # Cambiar por CarlosBatlles.pythonanywhere.com cuando queramos probar con la webapp
 Token=None
-def menu():
+
+def menu_principal():
     print("\n Bienvenido a RentAcar:")
-    print("1. Login")
-    print("2. Signup")
+    print("1. Iniciar sesión como Cliente")
+    print("2. Iniciar sesión como Administrador")
     print("3. Entrar como invitado")
-    print("4. Buscar Coches (como cliente)")
-    print("5. Alquilar Coches (como cliente)")
-    print("6. Ver Historial de Alquileres (como cliente)")
-    print("7. Gestionar coches (como admin)")
-    print("8. Consultar Bases de Datos (como admin)")
-    print("9. Generar informes (como admin)")
-    print("10. Buscar Coches (como invitado)")
-    print("11. Alquilar Coches (como invitado)")
     print("0. Salir")
 
-
-acciones = { #Aquí deben estar los nombres de las funciones de este archivo
-    "1": login,
-    "2": signup,
+acciones_principales = {
+    "1": iniciar_sesion_cliente,
+    "2": iniciar_sesion_admin,
     "3": entrar_como_invitado,
-    "4": buscar_coches_cliente,
-    "5": alquilar_coches_cliente,
-    "6": ver_historial_alquiler_cliente,
-    "7": gestionar_coches_admin,
-    "8": consultar_base_datos_admin,
-    "9": generar_informes_admin,
-    "10": buscar_coches_invitado,
-    "11": alquilar_coches_invitado,
 }
+
+def menu_cliente():
+    print("\n Menú Cliente:")
+    print("1. Buscar Coches")
+    print("2. Alquilar Coches")
+    print("3. Ver Historial de Alquileres")
+    print("0. Volver al Menú Principal")
+
+acciones_cliente = {
+    "1": registro_usuario()
+    "2": iniciar_sesion()
+    "3": ver_detalles_usuario()
+    "4": actualizar_contraseña_usuario()
+    "5": alquilar_coche()
+    "6": ver_detalles_alquiler_usuario()
+    "7": finalizar_alquiler_usuario()
+}
+
+def menu_admin():
+    print("\n Menú Administrador:")
+    print("1. Gestionar coches")
+    print("2. Consultar Bases de Datos")
+    print("3. Generar informes")
+    print("0. Volver al Menú Principal")
+
+acciones_admin = {
+
+"1": eliminar_usuario,
+"2": listar_usuarios,
+"3": registrar_coche,
+"4": actualizar_coche,
+"5": eliminar_coche,
+"6": listar_alquileres,
+"7": ver_detalles_alquiler_admin,
+"8": finalizar_alquiler_admin,
+
+}
+
+acciones_invitado = {
+    "1": entrar_como_invitado,
+    "2": signup,
+    # resto de funciones relacionadas con los invitados
+}
+# Funciones de autenticación y de la API
 
 def login():
     global Token
@@ -40,22 +70,58 @@ def login():
     r = requests.get(f"{BASE_URL}/login", params={"user": user, "passwd": passwd})
     print("Respuesta:", r.status_code, r.json())
     if r.status_code == 200:
-        TOKEN = r.json()["access_token"]
+        Token = r.json()["access_token"]
+        print(f"Bienvenido, {user}!")
+    else:
+        print("Error al iniciar sesión.")
 
 def signup():
     user = input("Usuario nuevo: ")
     passwd = input("Contraseña: ")
     r = requests.post(f"{BASE_URL}/signup", json={"user": user, "passwd": passwd})
     print("Respuesta:", r.status_code, r.json())
+
+def iniciar_sesion_cliente():
+    print("Iniciando sesión como Cliente...")
+    login()
+    while True:
+        menu_cliente()
+        opcion = input("Elige una opción: ")
+        if opcion == "0":
+            break
+        accion = acciones_cliente.get(opcion)
+        if accion:
+            accion()
+        else:
+            print("Opción no válida.")
+
+def iniciar_sesion_admin():
+    print("Iniciando sesión como Administrador...")
+    login()
+    while True:
+        menu_admin()
+        opcion = input("Elige una opción: ")
+        if opcion == "0":
+            break
+        accion = acciones_admin.get(opcion)
+        if accion:
+            accion()
+        else:
+            print("Opción no válida.")
+
+def entrar_como_invitado():
+    pass
+
 # Resto de endpoints
+
 if __name__ == "__main__":
     while True:
-        menu()
+        menu_principal()
         opcion = input("Elige una opción: ")
         if opcion == "0":
             print("👋 Saliendo...")
             break
-        accion = acciones.get(opcion)
+        accion = acciones_principales.get(opcion)
         if accion:
             accion()
         else:
