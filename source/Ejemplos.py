6b508741d7642f49537fe98a1487d7046477ee38
oscar125321313 <197656092+Oscar125841@users.noreply.github.com>
2025-04-10 22:49:52 +0200

    Menu invitados cambiado

--- source/Ejemplos.py
+++ source/Ejemplos.py
@@ -360,7 +360,36 @@ def entrar_como_invitado() -> None:
     global ROL
     ROL = "invitado"
     print("\nHas entrado como invitado.")
-    menu_cliente()  # Los invitados pueden usar el menú de cliente
+    while True:
+        print("\n--- Menú de Invitado ---")
+        print("1. Alquilar coche")
+        print("2. Buscar coches disponibles")
+        print('3. Datos usuario')
+        print('4. Actualizar contraseña')
+        print('5. Obtener detalles de un coche')
+        print('6. Categorias de coche')
+        print('7. Categorias de precio')
+        print("8. Volver al menú principal")
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            alquilar_coche()
+        elif opcion == "2":
+            buscar_coches_disponibles()
+        elif opcion == "3":
+            detalles_usuario()
+        elif opcion == '4':
+            actualizar_contraseña()
+        elif opcion == '5':
+            detalles_coche()
+        elif opcion == '6':
+            listar_tipos()
+        elif opcion == '7':
+            listar_precios()
+        elif opcion == '8':
+            break
+        else:
+            print("Opción no válida. Inténtalo de nuevo.")
 
 def registrar_coche() -> None:
     """
